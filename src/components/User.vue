e38dc45022ebbbd499f5be5d24da102fde7dc9b1
unknown <QWEPQ>
2020-03-11 00:25:09 +0800

    commit

--- src/components/User.vue
+++ src/components/User.vue
@@ -3,11 +3,11 @@
     <div class="user-title">用户管理</div>
     <div>
       <el-table :data="tableData" style="width: 100%">
-        <el-table-column prop="date" label="账户" width="180"></el-table-column>
-        <el-table-column prop="name" label="推荐人" width="180"></el-table-column>
-        <el-table-column prop="name" label="当前单量" width="180"></el-table-column>
-        <el-table-column prop="name" label="总收益" width="180"></el-table-column>
-        <el-table-column prop="address" label="账号状态"></el-table-column>
+        <el-table-column prop="phone_num" label="账户" width="180"></el-table-column>
+        <el-table-column prop="alipayName" label="推荐人" width="180"></el-table-column>
+        <el-table-column prop="password" label="当前单量" width="180"></el-table-column>
+        <el-table-column prop="password" label="总收益" width="180"></el-table-column>
+        <el-table-column prop="status" label="账号状态"></el-table-column>
         <el-table-column label="操作">
           <template slot-scope="scope">
             <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
